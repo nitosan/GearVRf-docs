02e23ceed65db2618487f9d993c3b959191b1948
Nite <nite.luo@samsung.com>
2017-08-30 15:59:55 -0700

    Create publishing script

--- publish.py
+++ publish.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 
+import os
+import shutil
+
 
 def main():
-    print 'main'
+    # Generate site with mkdocs
+    from subprocess import call
+    call(['mkdocs','build'])
+    print '==> Generating Documentation site'
+    # Copy api_reference and replace the placeholder api_reference in site
+    if os.path.isdir('site'):
+        if os.path.isdir('site/api_reference'):
+            shutil.rmtree('site/api_reference')
+        shutil.copytree('api_reference', 'site/api_reference')
+        print '==> Add API reference'
 
 if __name__ == "__main__":
     main()
